3860b43eee930efbb304bdde236bf5c67c734d00
lakshayvaishnav <123557766+lakshayvaishnav@users.noreply.github.com>
2024-01-07 21:04:43 +0530

    first commit

---
+++ assignment-1/index.ts
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 3000;
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+mongoose.connect(
+  "mongodb+srv://thelakshayvaishnav:1dUo1DnEvfnSvRPT@cluster0.ziuxmyd.mongodb.net/courses"
+);
